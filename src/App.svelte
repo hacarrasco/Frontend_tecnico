ce9958d0aa0b968c869d3fb381f5f77f850b3d7e
hacarrasco <Hugo.aCasstillo@gmail.com>
2023-06-08 15:45:48 -0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,114 @@
+<script>
+    import supabase from "../db";
+    import { onMount } from "svelte";
+    import Todo from "./component/Todo.svelte";
+  
+    let todos = [];
+    let newTodo = "";
+  
+    onMount(async () => {
+      await getAllTodos();
+    });
+  
+    const getAllTodos = async () => {
+      try {
+        let { data, error } = await supabase.from("todos").select("*");
+        todos = data;
+      } catch (error) {
+        // @ts-ignore
+        console.log(err);
+      }
+    };
+  
+    const addNewTodo = async () => {
+      try {
+        const { data, error } = await supabase
+          .from("todos")
+          .insert([{ titlem: newTodo }]);
+        await getAllTodos();
+        newTodo = "";
+      } catch (error) {
+        // @ts-ignore
+        console.log(err);
+      }
+    };
+  
+    const updateTodo = async (todo) => {
+      try {
+        const { data, error } = await supabase
+          .from("todos")
+          .update({ titlem: todo.titlem, state: todo.state })
+          .eq("id", todo.id);
+        await getAllTodos();
+      } catch (error) {
+        // @ts-ignore
+        console.log(err);
+      }
+    };
+  
+    const deleteTodo = async (todo) => {
+      try {
+        const { data, error } = await supabase
+          .from("todos")
+          .delete()
+          .eq("id", todo.id);
+        await getAllTodos();
+      } catch (error) {
+        // @ts-ignore
+        console.log(err);
+      }
+    };
+  
+    const handleKeyPress = (event) => {
+      if (event.key === "Enter" && newTodo !== "") {
+        addNewTodo();
+      }
+    };
+  
+  </script>
+  
+  <div class="col-lg-12" style="margin: 200px 0 0 350px;">
+    <div class="row col-md-6">
+      <div class="card" style="background-color: rgb(31, 41, 55);">
+        <div class="card-body" style="height: auto;">
+          <h3 style="color: #fff; padding-bottom:20px">TaskMaster</h3>
+          <div style="display: flex;">
+            <input
+              class="col-md-10 col-sm-2"
+              placeholder="Add a new todo..."
+              style="background: rgb(55, 65, 81); color:#fff"
+              type="text"
+              bind:value={newTodo}
+            />
+            <button class="btn btn-primary col-md-2" on:click={() => addNewTodo()}
+              >Add</button
+            >
+          </div>
+          <br />
+  
+          {#each todos as todo}
+            <Todo {todo} {updateTodo} {deleteTodo} />
+          {:else}
+            <p>No hay tareas</p>
+          {/each}
+          <div class="col-md-12" style="display: inline-flex;">
+            <p>Total Todos: {todos.length} |</p>
+            <p style="margin-left:5px ;">Completed Todos: 0</p>
+          </div>
+  
+          <p style="color:#fff; font-style:italic">
+            "Believe you can and you're halfway there." - Theodore Roosevelt
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <svelte:window on:keypress={handleKeyPress} />
+  
+  <style>
+    p {
+      color: #fff;
+    }
+  </style>
+  
---
+++ src/component/Todo.svelte
@@ -0,0 +1,74 @@
+<script>
+    export let todo, updateTodo, deleteTodo;
+    // @ts-ignore
+    import Fa from "svelte-fa/src/fa.svelte";
+  // @ts-ignore
+  import { faTrashCan, faCheck } from '@fortawesome/free-solid-svg-icons'
+  
+    export let itsComplete = false;
+  
+    const todoComplete = (todo) => {
+     if(todo.state !== itsComplete) {
+      itsComplete = !itsComplete
+     } 
+      }
+    
+  </script>
+  
+  <div>
+    <div class="todos">
+      <input
+        class={itsComplete ? 'active' : ''}
+        style="background: rgb(55, 65, 81); border-color:transparent; color:#fff; border-radius: 3px"
+        type="text"
+        value={todo.titlem}
+        on:input={(e) => {
+          todo.titlem = e.currentTarget.value;
+          updateTodo(todo);
+        }}
+        disabled
+      />
+  
+      <button class="btn1"  type="button"
+         on:click={todoComplete}
+      >
+        <Fa icon={faCheck}/>
+      </button>
+  
+  
+      <button class="btn" type="button"
+        on:click={() => deleteTodo(todo)}>
+        <Fa icon={faTrashCan} /> 
+      </button>
+    </div>
+    <br />
+  </div>
+  
+  <style>
+    
+    .todos {
+      width: 400px;
+      background: rgb(55, 65, 81);
+      display: flex;
+      margin-bottom: 0.25em;
+      justify-content: center;
+      gap: 15px;
+      margin-left: 50px;
+    }
+    .btn1{
+      background-color: rgb(55, 65, 81);
+      color: green;
+      border: none;
+    }
+    .btn{
+      background-color: rgb(55, 65, 81);
+      color: red;
+      border:none
+    }
+  
+    .active{
+      opacity:0.5;
+      text-decoration: line-through;
+    }
+  </style>
+  
